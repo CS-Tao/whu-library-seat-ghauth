f227c5b83af51de2305ecbd8c7336c40bedaf484
CS-Tao <990296951@qq.com>
2019-04-07 16:21:14 +0800

    Anti Xss

--- routes/loginComfirm.js
+++ routes/loginComfirm.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var logger = require('../helpers/logger');
+var antiXss = require('../helpers/antiXss')
 var router = express.Router();
 
 /* GET home page. */
@@ -7,7 +8,7 @@ router.get('/', function(req, res, next) {
   // 确认是否登录，用于本地存储 socketid
   var socketId = req.query.socketid;
   var device = req.query.device;
-  if (socketId && device) {
+  if (socketId && device && antiXss.validateDevice(device) && antiXss.validateSocketId(socketId)) {
     res.render('auth_comfirm', {
       title: '武汉大学图书馆抢座软件',
       socketId: socketId,
@@ -15,12 +16,12 @@ router.get('/', function(req, res, next) {
     });
     return
   } else {
-    logger.error('loginComfirm.js', '参数不足');
+    logger.error('loginComfirm.js', '参数错误');
     res.render('auth_error', {
       title: '武汉大学图书馆抢座软件',
       status: 'Error!',
       statusColorClass: 'band-red',
-      statusText: '错误信息：认证失败，参数不足',
+      statusText: '错误信息：认证失败，参数错误',
       statusText2: '请打开软件尝试重新登录'
     })
   }
